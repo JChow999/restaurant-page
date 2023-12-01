73a2aa0a4e317050708842471c96f3b85549b7c0
Jason <123558105+JChow999@users.noreply.github.com>
2023-12-01 18:02:15 -0500

    Add readme file. Minor improvements to contact page code.

---
+++ readme.md
@@ -0,0 +1,7 @@
+## Restaurant practice page ##
+This project is meant for dynamically rendering of a restaurant page while practicing fundamentals learnt for HTML, CSS, and Javascript.
+
+I wanted to continue practicing the flex box and grid, which I made sure to include them in the pages. Further, I wanted to learn other things such as the iframe.
+
+## Live site ##
+https://jchow999.github.io/restaurant-page/
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,9 +7,11 @@ function displayContact() {
     const content = document.querySelector('#content');
     const pageTitle = document.createElement('h2');
     const googleMap = document.createElement("iframe");
+    const mapTitle = document.createElement('h3');
 
 
     pageTitle.innerText = "Contact Us";
+    mapTitle.innerText = "Come visit us!"
 
     Object.assign(googleMap, {
         src:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.7477988419487!2d139.7428579759297!3d35.6585848312175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bbd9009ec09%3A0x481a93f0d2a409dd!2sTokyo%20Tower!5e0!3m2!1sen!2sca!4v1701290769011!5m2!1sen!2sca",
@@ -21,7 +23,8 @@ function displayContact() {
     content.innerHTML = '';
     content.appendChild(pageTitle);
     content.appendChild(createForm());
-    // content.appendChild(googleMap);
+    content.appendChild(mapTitle);
+    content.appendChild(googleMap);
 }
 
 export {displayContact};
