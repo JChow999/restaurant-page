44393927d086dcbe93849b2da7b59146a633a583
Jason <123558105+JChow999@users.noreply.github.com>
2023-12-01 17:25:12 -0500

    Adjust styling and code structure for better appearance

--- src/functions/createForm.js
+++ src/functions/createForm.js
@@ -4,6 +4,13 @@
  */
 function createForm() {
     const contactForm = document.createElement("form");
+    const formObj = {
+        fNameDiv : document.createElement('div'),
+        lNameDiv : document.createElement('div'),
+        emailDiv : document.createElement('div'),
+        commentDiv : document.createElement('div'),
+        submitDiv : document.createElement('div'),
+    }
     const fNameLabel = document.createElement('label');
     const fNameInput = document.createElement('input');
     const lNameLabel = document.createElement('label');
@@ -11,29 +18,45 @@ function createForm() {
     const emailLabel = document.createElement('label');
     const emailInput = document.createElement('input');
     const commentLabel = document.createElement('label');
-    const commentInput = document.createElement('input');
+    const commentInput = document.createElement('textarea');
+
     const submitBtn = document.createElement('input');
 
+
     fNameLabel.innerText = 'First Name:';
+    fNameLabel.setAttribute('for', 'first-name');
+
+    fNameInput.setAttribute('type', 'text')
+    fNameInput.setAttribute('name', 'first-name');
+    fNameInput.setAttribute('id', 'first-name');
+
     lNameLabel.innerText = 'Last Name:';
 
     emailLabel.innerText = 'Email:'
     emailLabel.setAttribute('type', 'email')
 
     commentLabel.innerText = 'Comment:';
-    commentInput.setAttribute('type', 'text');
+    commentLabel.id = "comment-label"
+    commentLabel.setAttribute('for', 'comment-box');
 
+    commentInput.setAttribute('name', 'comment-box');
+    commentInput.setAttribute('id', 'comment-box');
+
+    formObj.submitDiv.className = 'flex-center';
+
+    submitBtn.className = 'submit-btn';
     submitBtn.setAttribute('type', 'submit');
+    
+    formObj.fNameDiv.appendChild(fNameLabel);
+    formObj.fNameDiv.appendChild(fNameInput);
 
-    contactForm.appendChild(fNameLabel);
-    contactForm.appendChild(fNameInput);
-    contactForm.appendChild(lNameLabel);
-    contactForm.appendChild(lNameInput);
-    contactForm.appendChild(emailLabel);
-    contactForm.appendChild(emailInput);
-    contactForm.appendChild(commentLabel);
-    contactForm.appendChild(commentInput);
-    contactForm.appendChild(submitBtn);
+    formObj.submitDiv.appendChild(submitBtn);
+
+    for (const key in formObj) {
+        if (formObj[key] != '') {
+            contactForm.appendChild(formObj[key]);
+        }
+    }
     
     return contactForm;
 }
