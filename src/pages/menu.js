8e47e1781beb358424b88437afc829cfc95a4a20
Jason <123558105+JChow999@users.noreply.github.com>
2023-12-06 12:31:06 -0500

    Update menu

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -13,15 +13,20 @@ function displayMenu() {
 
     const mainMenuItems = [
         {name: 'Chicken Sandwich', price: 8.90, calories: 120, 
-        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ethanchlebowski.com%2Fcooking-techniques-recipes%2Fthe-original-chicken-sandwich-from-chick-fil-a&psig=AOvVaw2cW90k2JW9nvpcEJIwdEG1&ust=1701796213166000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNixi5Oj9oIDFQAAAAAdAAAAABAE'},
-        {name: 'Beef Sandwich', price: 10.90, calories: 140, image: null},
-        {name: 'Beef and Chicken Sandwich', price: 8.90, calories: 120, image: null},
+        image: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2023/09/mayo-chicken-sandwich.jpg.webp'},
+        {name: 'Beef Sandwich', price: 10.90, calories: 140, 
+        image: 'https://www.tasteofhome.com/wp-content/uploads/2018/01/Savory-Beef-Sandwiches_EXPS_HSCBZ17_9191_B07_27_4b.jpg?fit=700%2C1024'},
+        {name: 'Beef and Chicken Sandwich', price: 8.90, calories: 120, 
+        image: 'https://somethingaboutsandwiches.com/wp-content/uploads/2021/04/grilled-chicken-sandwich.jpg'},
     ];
     
     const drinkMenuItems = [
-        {name: 'Coffee', price: 3.50, calories: 5, image: null},
-        {name: 'Hong Kong Milk Tea', price: 5.50, calories: 5, image: null},
-        {name: 'Juice', price: 2.50, calories: 20, image: null},
+        {name: 'Coffee', price: 3.50, calories: 5, 
+        image: 'https://insanelygoodrecipes.com/wp-content/uploads/2020/07/Cup-Of-Creamy-Coffee-1024x536.webp'},
+        {name: 'Hong Kong Milk Tea', price: 5.50, calories: 5, 
+        image: 'https://carmyy.com/wp-content/uploads/2021/12/Hong-Kong-Milk-Tea-5.jpg'},
+        {name: 'Juice', price: 2.50, calories: 20, 
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Orange_juice_1.jpg/844px-Orange_juice_1.jpg'},
     ];
 
     mainSubtitle.innerText = "Mains";
@@ -30,15 +35,13 @@ function displayMenu() {
         const itemInfo = mainMenuItems[n];
 
         const menuBox = document.createElement('div');
-        const image = document.createElement('img');
-        const itemName = document.createElement('h3');
-
-        menuBox.className = "menu-item"
+        const itemName = document.createElement('h4');
+        
+        menuBox.className = "menu-item";
+        menuBox.style.backgroundImage = `url(${itemInfo.image})`;
         itemName.innerText = itemInfo.name;
 
-        menuBox.appendChild(image);
         menuBox.appendChild(itemName);
-
         menuItems.append(menuBox);
     }
 
@@ -48,13 +51,12 @@ function displayMenu() {
         const itemInfo = drinkMenuItems[n];
 
         const menuBox = document.createElement('div');
-        const image = document.createElement('img');
-        const itemName = document.createElement('h3');
+        const itemName = document.createElement('h4');
 
-        menuBox.className = "menu-item"
+        menuBox.className = "menu-item";
+        menuBox.style.backgroundImage = `url(${itemInfo.image})`;
         itemName.innerText = itemInfo.name;
 
-        menuBox.appendChild(image);
         menuBox.appendChild(itemName);
 
         menuItems.append(menuBox);
